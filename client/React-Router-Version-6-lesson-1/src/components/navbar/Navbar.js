ff5c921e0634eca91691bada2fb3d6e3abf9b339
isaac <isaacd4444@gmail.com>
2022-09-11 14:27:33 -0400

    conditional navbar

--- client/React-Router-Version-6-lesson-1/src/components/navbar/Navbar.js
+++ client/React-Router-Version-6-lesson-1/src/components/navbar/Navbar.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import { Link } from 'react-router-dom'
+import { AuthContext } from '../../context/AuthContext'
+
 
 export default function Navbar() {
+  const {isAuth}=useContext(AuthContext);
   return (
     <nav>
       <h2>College Planner</h2>
-      <Link to='/dashboard'>Dashboard</Link>
-      <Link to='/create'>Create</Link>
-      <Link to='/signup'>Signup</Link>
-      <Link to='/signin'>Signin</Link>
+      {isAuth && <Link to='/dashboard'>Dashboard</Link>}
+      {isAuth &&<Link to='/create'>Create</Link>}
+      {!isAuth && <Link to='/signup'>Signup</Link>}
+      {!isAuth && <Link to='/signin'>Signin</Link>}
      </nav>
   )
 }
